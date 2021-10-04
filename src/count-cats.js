67f95a78ad2bffdf632151fca4c59a0ffd8e347b
Karina Zaitseva <>
2021-10-05 01:11:59 +0300

    feat: add count cats task

--- src/count-cats.js
+++ src/count-cats.js
@@ -14,7 +14,10 @@ import { NotImplementedError } from '../extensions/index.js';
  * ]) => 3`
  *
  */
-export default function countCats(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function countCats(matrix) {
+  return matrix.reduce((previousValue, currentValue) => {
+    return (currentValue || []).filter((item) => {
+      return item === '^^';
+    }).length + previousValue;
+  }, 0);
 }
